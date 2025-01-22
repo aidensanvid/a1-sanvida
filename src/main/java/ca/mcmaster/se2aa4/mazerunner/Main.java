0066d5f848d1818ea666bff3f36f4a195770eb4f
aidensanvido <aidensanvido@gmail.com>
2025-01-22 13:23:02 -0500

    Identified and implemented key object abstractions

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
@@ -2,13 +2,106 @@ package ca.mcmaster.se2aa4.mazerunner;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.DefaultParser;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+class Maze {
+
+    private char[][] maze;
+    private int rows;
+    private int cols;
+
+    public int[] getMazeDimensions(String maze_file) throws FileNotFoundException, IOException{
+
+        int[] dimensions = new int[2];
+
+        int rows = 0;
+        int cols = 0;
+
+        BufferedReader reader = new BufferedReader(new FileReader(maze_file));
+        String line;
+
+        while ((line = reader.readLine()) != null) {
+            if (cols == 0) {
+                cols = line.length();
+            }
+            rows++;
+        }
+
+        dimensions[0] = rows;
+        dimensions[1] = cols;
+
+        return dimensions;
+
+    }
+
+    public char[][] loadMaze(String maze_file) throws FileNotFoundException, IOException {
+
+        char[][] maze = new char[rows][cols];
+
+        BufferedReader reader = new BufferedReader(new FileReader(maze_file));
+        String line;
+
+        int row = 0;
+        int col = 0;
+
+        while ((line = reader.readLine()) != null) {
+            for (int idx = 0; idx < line.length(); idx++) {
+                maze[row][col] = line.charAt(idx);
+                col++;
+            }
+            col = 0;
+            row++;
+        }
+
+        return maze;
+    }
+
+    public void displayMaze(){
+
+        for (int row = 0; row<rows; row++) {
+            for (int col = 0;col<cols;col++) {
+                System.out.print(maze[row][col]);
+            }
+            System.out.println();
+        }
+
+
+    }
+
+    public Maze(String maze_file) throws FileNotFoundException, IOException{
+
+        int[] dimensions = getMazeDimensions(maze_file);
+
+        this.rows = dimensions[0];
+        this.cols = dimensions[1];
+        this.maze = loadMaze(maze_file);
+
+    }
+
+}
+
+class MazeExplorer {
+
+    // Maze will be its own class
+    // Inside the maze class will 
+
+    private Maze maze;
+
+    public MazeExplorer(Maze maze){
+
+        this.maze = maze;
+
+    }
+
+}
 
 public class Main {
 
@@ -34,22 +127,11 @@ public class Main {
                 logger.info("Starting Maze Runner");
                 logger.info("Reading the maze from file " + maze_file);
                 
-                BufferedReader reader = new BufferedReader(new FileReader(maze_file));
+                Maze maze = new Maze(maze_file);
+                MazeExplorer explorer = new MazeExplorer(maze);
+                maze.displayMaze();
 
-                String line;
-
-                // Reading maze from selected file
-                while ((line = reader.readLine()) != null) {
-                    for (int idx = 0; idx < line.length(); idx++) {
-                        if (line.charAt(idx) == '#') {
-                            System.out.print("WALL ");
-                        } else if (line.charAt(idx) == ' ') {
-                            System.out.print("PASS ");
-                        }
-                    }
-                    System.out.print(System.lineSeparator());
-                    }
-                }
+            }
             else{
                 logger.error("No maze file provided.");
             }
